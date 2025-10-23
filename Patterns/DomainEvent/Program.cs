04875b07ec74618e0aaece33d99a74fcf5187106
Samuel797186 <103047851+Samuel797186@users.noreply.github.com>
2025-10-23 14:28:53 -0300

    Criando projeto estudos

---
+++ Patterns/DomainEvent/Program.cs
@@ -0,0 +1,181 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System.Collections.Concurrent;
+using System.Reflection;
+
+#region .: Criando Provider :.
+Console.WriteLine("###                   Evento de Domínio.                   ###");
+Console.WriteLine(new string('-', 80));
+Console.WriteLine();
+
+var services = new ServiceCollection();
+services.AddScoped<IDomainEventHandler<FaturaConciliadaEvent>, AtualizarSatatusNotaHandler>();
+services.AddScoped<INotaFiscalRepository, NotaFiscalRepository>();
+var serviceProvider = services.BuildServiceProvider();
+#endregion
+
+
+var fatura = new Fatura(1, 2, StatusConciliacao.EmAberto);
+
+// Atualizando Status Conciliação Fatura
+fatura.AtualizarStatusConciliacao(StatusConciliacao.PagamentoParcial);
+
+var dispatcher = new DomainEventDispatcher(serviceProvider);
+await dispatcher.PublicarAsync(fatura.Eventos);
+
+Console.ReadKey();
+
+#region .: Classes Evento :.
+public interface IDomainEvent
+{
+    DateTime DataOcorrencia { get; }
+}
+
+public interface IDomainEventHandler<in TEvent> where TEvent : IDomainEvent
+{
+    Task HandleAsync(TEvent dominioEvento);
+}
+
+public class FaturaConciliadaEvent : IDomainEvent
+{
+    public int FaturaId { get; set; }
+    public int NotaId { get; set; }
+    public StatusConciliacao StatusConciliacao { get; set; }
+    public DateTime DataOcorrencia => DateTime.UtcNow;
+
+    public FaturaConciliadaEvent(int faturaId, int notaId, StatusConciliacao statusConciliacao)
+    {
+        FaturaId = faturaId;
+        NotaId = notaId;
+        StatusConciliacao = statusConciliacao;
+    }
+}
+
+public class AtualizarSatatusNotaHandler : IDomainEventHandler<FaturaConciliadaEvent>
+{
+    private readonly INotaFiscalRepository _notaFiscalRepository;
+
+    public AtualizarSatatusNotaHandler(INotaFiscalRepository notaFiscalRepository)
+    {
+        _notaFiscalRepository = notaFiscalRepository;
+    }
+
+    public async Task HandleAsync(FaturaConciliadaEvent dominioEvento)
+    {
+        var nota = _notaFiscalRepository.ObterPorId(dominioEvento.NotaId);
+        Console.WriteLine($"[Nota Fiscal] #Buscando Nota Handler.");
+
+        if (nota is null)
+            return;
+
+        nota.AtualizarStatusConciliacao(dominioEvento.StatusConciliacao);
+        await _notaFiscalRepository.AtualizarAsync(nota);
+    }
+}
+
+public class DomainEventDispatcher
+{
+    private readonly IServiceProvider _serviceProvider;
+    private static readonly ConcurrentDictionary<Type, MethodInfo> _handleMetodos = [];
+
+    public DomainEventDispatcher(IServiceProvider serviceProvider)
+    {
+        _serviceProvider = serviceProvider;
+    }
+
+    public async Task PublicarAsync(IEnumerable<IDomainEvent> eventos)
+    {
+        Console.WriteLine($"[Publicando Eventos] #Dispachante de Eventos");
+        Console.WriteLine();
+        foreach (var evento in eventos)
+        {
+            var handlerTipo = typeof(IDomainEventHandler<>).MakeGenericType(evento.GetType());
+            var handlers = _serviceProvider.GetServices(handlerTipo);
+
+            foreach (var handler in handlers)
+            {
+                var handleMetodo = _handleMetodos.GetOrAdd(handlerTipo, tipo => 
+                                    tipo.GetMethod("HandleAsync") ?? 
+                                    throw new InvalidOperationException($"Método HandleAsync não encontrado. {tipo}"));
+
+                await (Task)handleMetodo.Invoke(handler, [evento])!;
+            }
+        }
+    }
+}
+#endregion
+
+#region .: Entidades Etc.. :.
+public class Fatura
+{
+    public int Id { get; set; }
+    public int NotaId { get; set; }
+    public StatusConciliacao StatusConciliacao { get; set; }
+
+    private readonly List<IDomainEvent> _eventos = [];
+    public IReadOnlyCollection<IDomainEvent> Eventos => _eventos.AsReadOnly();
+
+
+    public Fatura(int id, int notaId, StatusConciliacao statusConciliacao)
+    {
+        Id = id;
+        NotaId = notaId;
+        StatusConciliacao = statusConciliacao;
+    }
+
+    public void AtualizarStatusConciliacao(StatusConciliacao novoStatusConciliacao)
+    {
+        if (StatusConciliacao == StatusConciliacao.Pago)
+            return;
+
+        Console.WriteLine($"[Fatura] #Atualizando Status Conciliação da Fatura....");
+        StatusConciliacao = novoStatusConciliacao;
+
+        Console.WriteLine();
+        Console.WriteLine($"[Fatura] #Criando Evento....");
+        var evento = new FaturaConciliadaEvent(Id, NotaId, StatusConciliacao);
+
+        Console.WriteLine();
+        _eventos.Add(evento);
+    }
+
+    public void LimparEventos() => _eventos.Clear();
+}
+
+public class NotaFiscal(int Id, StatusConciliacao StatusConciliacao)
+{
+    public int Id { get; set; } = Id;
+    public StatusConciliacao StatusConciliacao { get; set; } = StatusConciliacao;
+
+    public void AtualizarStatusConciliacao(StatusConciliacao novoStatusConciliacao)
+        => StatusConciliacao = novoStatusConciliacao;
+}
+
+public interface INotaFiscalRepository
+{
+    Task AtualizarAsync(NotaFiscal notaFiscal);
+    NotaFiscal ObterPorId(int id);
+}
+
+public class NotaFiscalRepository : INotaFiscalRepository
+{
+    public Task AtualizarAsync(NotaFiscal notaFiscal)
+    {
+        Console.WriteLine();
+        Console.WriteLine($"[Nota Repository] #Atualizando NotaId: {notaFiscal.Id} - novo Status: {notaFiscal.StatusConciliacao}");
+        return Task.CompletedTask;
+    }
+
+    public NotaFiscal ObterPorId(int id)
+    {
+        return new NotaFiscal(id, StatusConciliacao.EmAberto);
+    }
+}
+
+public enum StatusConciliacao
+{
+    EmAberto = 1,
+    PagamentoParcial = 2,
+    Pago = 3
+}
+#endregion
+
---
+++ Patterns/ObserverPattern/Program.cs
@@ -0,0 +1,219 @@
+﻿
+#region .: Console Exemplo 1 :.
+//Console.WriteLine($"******* Exemplo 1 Pattern Observer *********");
+//Console.WriteLine(new string('-', 90));
+//Console.WriteLine();
+
+//var fatura = new Fatura(1, 100m, Status.EmAberto);
+//var nota = new NotaFisca();
+
+//fatura.Registrar(nota);
+//Console.WriteLine();
+
+//Console.WriteLine($"Fatura-> {(string)fatura}");
+
+//Console.WriteLine(new string('-', 90));
+
+//fatura.AtualizarStatus(Status.Paga);
+
+//Console.WriteLine(new string('-', 90));
+#endregion
+
+#region .: Console Exemplo 2 :.
+var bolsa = new BolsaDeValores(100.0m);
+
+var investidor1 = new Investidor("João");
+var investidor2 = new Investidor("Maria");
+var corretora = new Corretora();
+
+bolsa.Registrar(investidor1);
+bolsa.Registrar(corretora);
+bolsa.Registrar(investidor2);
+
+bolsa.MudarPreco(102.5m);
+bolsa.MudarPreco(99.8m);
+
+bolsa.Remover(investidor2);
+
+bolsa.MudarPreco(101.0m);
+#endregion
+
+
+Console.ReadLine();
+
+#region .: Classes Exemplo 1 :.
+public class NotaFisca : IObserver<Fatura>
+{
+    public int NotaId { get; set; } = 1;
+    public Status Status { get; set; }
+
+    public NotaFisca() { }
+
+    public NotaFisca(int notaId, Status status)
+    {
+        NotaId = notaId;
+        Status = status;
+    }
+
+    public void AtualizarStatus(Status novoStatus) => Status = novoStatus;
+
+    public void Update(Fatura evento)
+    {
+        if (evento is Fatura)
+            AtualizarStatus(evento.Status);
+        Console.WriteLine($"Nota Fiscal: {NotaId + evento.Id}  - Nota Status: {Status} - atualizada com sucesso......");
+    }
+}
+
+public class Fatura : ISubject<Fatura>
+{
+    private readonly List<IObserver<Fatura>> _observadores = [];
+
+    public int Id { get; set; }
+    public decimal Valor { get; set; }
+    public Status Status { get; set; }
+
+    public Fatura(int faturaId, decimal valor, Status status)
+    {
+
+        Id = faturaId;
+        Valor = valor;
+        Status = status;
+    }
+
+
+    public void Registrar(IObserver<Fatura> observadores)
+    {
+        Console.WriteLine($"*** Observador registrado com sucesso. ***.");
+        _observadores.Add(observadores);
+    }
+
+    public void AtualizarStatus(Status novoStatus)
+    {
+        if (Status != novoStatus)
+        {
+            Status = novoStatus;
+            Notificar();
+        }
+    }
+
+    public void Notificar()
+    {
+        foreach (var observer in _observadores)
+            observer.Update(this);
+    }
+
+    public void Remover(IObserver<Fatura> observer)
+    {
+        Console.WriteLine($"Observador Removido.");
+        _observadores.Remove(observer);
+    }
+
+    public static implicit operator string(Fatura fatura)
+        => $@"FaturaId: {fatura.Id} - Valor: {fatura.Valor} - Status: {fatura.Status}";
+}
+
+public interface IObserver<T>
+{
+    void Update(T sujeito);
+}
+
+public interface ISubject<T>
+{
+    void Registrar(IObserver<T> observador);
+    void Notificar();
+    void Remover(IObserver<T> observador);
+}
+
+public enum Status
+{
+    EmAberto = 1,
+    Paga = 2,
+    Cancelado = 3
+}
+#endregion
+
+
+#region .: Classe Exemplo 2 :.
+public class BolsaDeValores : ISujeito
+{
+    private readonly List<IObservador> _observadores = [];
+    public decimal PrecoAcao { get; private set; }
+
+    public BolsaDeValores(decimal precoAcao)
+    {
+        PrecoAcao = precoAcao;
+    }
+
+    public void MudarPreco(decimal novoPreco)
+    {
+        if (PrecoAcao != novoPreco)
+        {
+            PrecoAcao = novoPreco;
+            Console.WriteLine($"\nBolsa: O preço da ação mudou para {novoPreco:C}.");
+            Notificar();
+        }
+    }
+
+    public void Notificar()
+    {
+        foreach (var observador in _observadores)
+        {
+            observador.Atualizar(this);
+        }
+    }
+
+    public void Registrar(IObservador observador)
+    {
+        Console.WriteLine($"Observador anexado.");
+        _observadores.Add(observador);
+    }
+
+    public void Remover(IObservador observador)
+    {
+        Console.WriteLine($"Observador desanexado.");
+        _observadores.Remove(observador);
+    }
+}
+
+public class Investidor : IObservador
+{
+    private string _nome;
+
+    public Investidor(string nome)
+    {
+        _nome = nome;
+    }
+
+    public void Atualizar(ISujeito sujeito)
+    {
+        if (sujeito is BolsaDeValores bolsa)
+        {
+            Console.WriteLine($"Investidor {_nome}: Notificado! O novo preço da ação é {bolsa.PrecoAcao:C}.");
+        }
+    }
+}
+
+public class Corretora : IObservador
+{
+    public void Atualizar(ISujeito sujeito)
+    {
+        if (sujeito is BolsaDeValores bolsa)
+        {
+            Console.WriteLine($"Corretora: Atualizando dados... O preço atual é {bolsa.PrecoAcao:C}.");
+        }
+    }
+}
+
+public interface ISujeito
+{
+    void Registrar(IObservador observador);
+    void Notificar();
+    void Remover(IObservador observador);
+}
+
+public interface IObservador
+{
+    void Atualizar(ISujeito sujeito);
+}
+#endregion
